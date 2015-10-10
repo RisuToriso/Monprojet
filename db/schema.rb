730c3ed249d94dcde0c07cc12b4c181a65ff88b2
Alice <alicetrinh95@gmail.com>
2015-10-10 16:09:39 +0200

    device

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010092956) do
+ActiveRecord::Schema.define(version: 20151010133142) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20151010092956) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",               limit: 255, default: "", null: false
+    t.string   "encrypted_password",  limit: 255, default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
